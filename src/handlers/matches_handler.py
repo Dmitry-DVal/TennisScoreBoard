57398b77b10b85a5b5dedc23cdb0ab8cc8992059
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2025-03-03 22:32:37 +0400

    /matches displays all completed matches.

--- src/handlers/matches_handler.py
+++ src/handlers/matches_handler.py
@@ -1,10 +1,35 @@
-from src.handlers.base_handler import BaseHandler
+import urllib.parse
+
+from src.handlers.base_handler import RequestHandler, logger
+from src.services.match_service import MatchService
 
 
-class MatchesHandler(BaseHandler):
+class MatchesHandler(RequestHandler):
     """Обработчик страницы завершенных матчей."""
 
-    def handle_request(self, environ, start_response):
-        response_body = self.render_template("completed_matches.html")
-        start_response("200 OK", [("Content-Type", "text/html; charset=utf-8")])
-        return [response_body]
+    def __init__(self):
+        self.match_service = MatchService()
+
+    def handle_get(self, environ, start_response):
+        data = urllib.parse.parse_qs(environ.get("QUERY_STRING", ""))
+        logger.debug(f"Полученные данные в GET {data}")
+
+        if data:  # Если словарь не пустой
+            response_body = self.render_template("completed_matches.html")
+
+        else:
+
+            logger.debug(f"Делаем запрос всех завершенных матчей")
+            matches = self.match_service.get_completed_matches()
+            print(matches)
+
+            response_body = self.render_template("completed_matches.html",
+                                                 matches=matches)
+
+            return self.make_response(start_response, response_body)
+
+        return self.make_response(start_response, response_body)
+
+    def handle_post(self, environ, start_response):
+        return self.make_response(start_response, b"Method Not Allowed",
+                                  "405 Method Not Allowed")
--- src/services/match_service.py
+++ src/services/match_service.py
@@ -1,8 +1,11 @@
 import logging
 from uuid import uuid4
 
+from sqlalchemy.orm import aliased
+
 from src.database import session
 from src.models.match_model import MatchesOrm
+from src.models.player_model import PlayerOrm
 from src.services.scoring_service import Match
 
 logger = logging.getLogger("app_logger")
@@ -57,3 +60,32 @@ class MatchService:
                     {"Score": match_obj.to_dict()}
                 )
             db_session.commit()
+
+    def get_completed_matches(self):
+        """Получает все завершенные матчи."""
+        with (session() as db_session):
+            Player1 = aliased(PlayerOrm)
+            Player2 = aliased(PlayerOrm)
+            Winner = aliased(PlayerOrm)
+
+            matches = db_session.query(
+                Player1.Name,
+                Player2.Name,
+                Winner.Name,
+                MatchesOrm.Score['sets']).join(
+                Player1, MatchesOrm.Player1 == Player1.ID).join(
+                Player2, MatchesOrm.Player2 == Player2.ID).join(
+                Winner, MatchesOrm.Winner == Winner.ID).filter(
+                MatchesOrm.Winner.isnot(None)).all()
+            logger.debug(type(matches))
+            print()
+            print(matches[0],
+                  type(matches[0]))  # ...  <class 'sqlalchemy.engine.row.Row'>
+            print()
+            print(matches[0][0])  # Zhirna Mraz
+            print(matches[0][1])  # Dima
+            print(matches[0][2])  # Dima
+            print(matches[0][3])  # [1, 2]
+            print(len(matches))
+
+            return matches
